19ceaaebc2d6e8655a360b504dd695c670933827
nawaf <n.alhu10@gmail.com>
2023-11-24 22:10:22 +0300

    complete the function and View Reservation tab

--- User.py
+++ User.py
@@ -3,70 +3,105 @@ import Sing_UP
 from tkinter import ttk
 from tkcalendar import Calendar
 from tkinter import messagebox
+from DB import *
 
 Color = "#48A3BC"
 Font = ('Arial', 12, 'bold')
 
 class User:
-    def __init__(self):
+    def __init__(self, ID, User_Class):
         self.window = tk.Tk()
         self.window.title("User Window")
         self.window.minsize(width="900", height="450")
         self.window.config(padx=10, pady=80, bg=Color)
 
+        self.StudentID = ID
+        self.User_Class = User_Class
+
         # Create a Tabs
-        self.Tabs = ttk.Notebook(self.window)
-        self.Tabs.pack(padx=10, pady=10)
+        self.Tabs = ttk.Notebook(self.window, width=800, height=350)
+        self.Tabs.pack(padx=20, pady=20)
         # Reserve a Golf Carts Tab
-        self.Reserve_Tab = ttk.Frame(self.Tabs, width=800, height=350)
+        self.Reserve_Tab = ttk.Frame(self.Tabs, width=900, height=450)
         self.Reserve_Tab.pack(fill="both", expand=1)
         self.Tabs.add(self.Reserve_Tab, text="Reserve a Golf Carts")
         # View my Reservations Tab
-        self.View_Tab = ttk.Frame(self.Tabs, width=800, height=350)
+        self.View_Tab = ttk.Frame(self.Tabs, width=900, height=450)
         self.View_Tab.pack(fill="both", expand=1)
         self.Tabs.add(self.View_Tab, text="View my Reservations")
 
-        treeview_reserve = ttk.Treeview(self.Reserve_Tab, columns=("College",), show="headings")
-        treeview_reserve.heading("College", text="College")
-        colleges = ("Business-Administration", "Architecture-and-Planning", "Computer-and-Information-Sciences", "Food-and-Agricultural-Sciences", "Engineering", "Science")
-        treeview_reserve.insert("", "0", "item1", text="Item 1", values=colleges[0])
-        treeview_reserve.insert("", "1", "item2", text="Item 2", values=colleges[1])
-        treeview_reserve.insert("", "2", "item3", text="Item 3", values=colleges[2])
-        treeview_reserve.insert("", "3", "item4", text="Item 4", values=colleges[3])
-        treeview_reserve.insert("", "4", "item5", text="Item 5", values=colleges[4])
-        treeview_reserve.insert("", "5", "item6", text="Item 6", values=colleges[5])
-        treeview_reserve.pack(side="left", padx=10, pady=10)
+        self.Treeview_reserve = ttk.Treeview(self.Reserve_Tab)
+        
+        # Define our columns
+        self.Treeview_reserve['columns'] = ('College', 'Golf Cart Plate')
+        
+        # Create Columns
+        self.Treeview_reserve.column("#0", width=0)
+        self.Treeview_reserve.column("College", anchor='w', width=150)
+        self.Treeview_reserve.column("Golf Cart Plate", anchor='w', width=150)
+
+        # Create Heading
+        self.Treeview_reserve.heading("#0", text="")
+        self.Treeview_reserve.heading("College", text="College", anchor='w')
+        self.Treeview_reserve.heading("Golf Cart Plate", text="Golf Cart Plate", anchor='w')
+
+        colleges = ('Business Administration', "Architecture and Planning", "Computer Sciences", "Agricultural Sciences", "Engineering", "Science")
+        
+        # insert Data
+        id = 0
+        for Row in Cursor.execute("""SELECT * FROM GOLF""") :
+            self.Treeview_reserve.insert('', index='end', iid=id, text='', values=(Row[1], Row[0]))
+            id += 0
+        self.Treeview_reserve.pack(side="left", padx=10, pady=10)
 
         # TreeView for View my Reservations Tab
-        treeview_view = ttk.Treeview(self.View_Tab, columns=("College", "Golf Cart", "Date"), show="headings")
-        treeview_view.heading("College", text="College")
-        treeview_view.heading("Golf Cart", text="Golf Cart")
-        treeview_view.heading("Golf Cart", text="Golf Cart")
-        treeview_view.heading("Date", text="Date")
-        treeview_view.pack()
+        self.Treeview_view = ttk.Treeview(self.View_Tab)
+        self.Treeview_view['columns'] = ("College", "Golf Cart Plate", "Date")
+        self.Treeview_view.column("#0", width=0)
+        self.Treeview_view.column("College", width=150, anchor='w')
+        self.Treeview_view.column("Golf Cart Plate", width=150, anchor='center')
+        self.Treeview_view.column("Date", width=150, anchor='w')
+
+        # heading
+        self.Treeview_view.heading("#0", text="")
+        self.Treeview_view.heading("College", text="College")
+        self.Treeview_view.heading("Golf Cart Plate", text="Golf Cart Plate")
+        self.Treeview_view.heading("Date", text="Date")
 
         # Calender
         self.calender = Calendar(self.Reserve_Tab, selectmode="day", year=2023, month=1, date=1)
-        self.calender.place(x=250, y=0)
+        self.calender.place(x=350, y=80)
 
         # Time
-        Time = ['8:00-8:30', '8:30-9:00', '9:00-9:30', '9:30-10:00', '10:00-10:30', '10:30-11:00', '11:00-11:30', '11:30-12:00',
-                '12:00-12:30', '12:30-13:00', '13:00-13:30', '13:30-14:00']  # options
-        self.Times = ttk.Combobox(self.Reserve_Tab, values=Time, width=10)  # Combobox
-        self.Times.set(Time[0])
+        Student_Time = ['8:00-8:30', '8:30-9:00', '9:00-9:30', '9:30-10:00', '10:00-10:30', '10:30-11:00', '11:00-11:30', '11:30-12:00',
+                '12:00-12:30', '12:30-13:00', '13:00-13:30', '13:30-14:00']
+        Faculty_Time = ['8:00-9:00', '9:00-10:00', '10:00-11:00', '11:00-12:00', '12:00-13:00', '13:00-14:00']
+        Employee_Time = ['8:00-9:30', '9:30-11:00', '11:00-12:30', '12:30-14:00']
 
-        self.Times.place(x=510, y=20)
+        if self.User_Class == 'Student' :
+            self.Times = ttk.Combobox(self.Reserve_Tab, values=Student_Time, width=10)
+        elif self.User_Class == 'Faculty' :
+            self.Times = ttk.Combobox(self.Reserve_Tab, values=Faculty_Time, width=10)
+        elif self.User_Class == 'Employee' :
+            self.Times = ttk.Combobox(self.Reserve_Tab, values=Employee_Time, width=10)
 
-        # Book button
-        self.Button1 = tk.Button(self.Reserve_Tab, text="Reserve", width=10, height=1)
-        self.Button1.place(relx=.45, rely=.80)
+        self.Times.set(Student_Time[0])
+        self.Times.place(x=610, y=80)
+
+        # Select Date Button
+        self.Select_Button = tk.Button(self.Reserve_Tab, text='Select Date', width=10, height=1, command=self.Date)
+        self.Select_Button.place(relx=.60, rely=.90)
+
+        # Reserve button
+        self.Reserve_Button = tk.Button(self.Reserve_Tab, text="Reserve", width=10, height=1, command=self.Reserve)
+        self.Reserve_Button.place(relx=.15, rely=.90)
 
         # view button
-        self.Button3 = tk.Button(self.View_Tab, text="Show", width=10, height=1)
-        self.Button3.place(relx=.45, rely=.80)
+        self.View_Button = tk.Button(self.View_Tab, text="Show", width=10, height=1, command=self.ViewMyReservations)
+        self.View_Button.place(relx=.45, rely=.80)
 
         # Back Button
-        self.buttonBack = tk.Button(self.window, text='Logout', command=self.logout)
+        self.buttonBack = tk.Button(self.window, text='Logout', width=10, height=1, command=self.logout)
         self.buttonBack.pack()
 
         self.window.mainloop()
@@ -74,9 +109,32 @@ class User:
     def logout(self):
         self.window.destroy()
         Sing_UP.sing_up()
-    
-    def create_tab(self, tab_name):
-        tab = ttk.Frame(self.Tabs)
-        self.Tabs.add(tab, text=tab_name)
 
-User()
+    def Date(self):
+        self.Dates = f"{self.Times.get()}, {self.calender.get_date()}"
+        messagebox.showinfo("Done", "Date Selected Successfully")
+
+    def Reserve(self):
+        try:
+            self.Details = self.Treeview_reserve.item(self.Treeview_reserve.focus())
+            College_Selected = self.Details.get("values")[0]
+            Golf_Selected = self.Details.get("values")[1]
+            Cursor.execute("""INSERT INTO RESERVATIONS (StudentID, COLLEGE, GolfPlate, Date)
+                              VALUES (?, ?, ?, ?)""", (self.StudentID, College_Selected, Golf_Selected, self.Dates))
+            Connection.commit()
+            messagebox.showinfo('Done', 'Your Reservation is Complete')
+        except AttributeError:
+            messagebox.showerror("Error", "Select Date First")
+        except IndexError :
+            messagebox.showerror('Error', 'Please Select a Golf Cart')
+        except sqlite3.IntegrityError :
+            messagebox.showerror("Error", "Select Another Date and Golf Cart")
+
+    def ViewMyReservations(self):
+        count = 0
+        for Reserve in Cursor.execute(f"""SELECT * FROM RESERVATIONS WHERE StudentID == '{self.StudentID}' """) :
+            self.Treeview_view.insert('', index='end', iid=count, text='', values=(Reserve[1], Reserve[2], Reserve[3]))
+            count += 1
+        self.Treeview_view.pack(padx=10, pady=10)
+
+User(112,'Student')
